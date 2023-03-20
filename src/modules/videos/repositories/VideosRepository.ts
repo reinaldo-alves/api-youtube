9d26f38b61430521d3e62051873b0d97079bafa3
Reinaldo Alves <reinaldoasjr8@gmail.com>
2023-03-20 15:04:24 -0300

    create video integration

--- src/modules/videos/repositories/VideosRepository.ts
+++ src/modules/videos/repositories/VideosRepository.ts
@@ -1,16 +1,14 @@
 import { pool } from '../../../mysql';
 import { v4 as uuidv4 } from 'uuid';
-import { hash, compare } from 'bcrypt';
-import { sign } from 'jsonwebtoken';
 import { Request, Response } from 'express';
 
 class VideoRepository {
     create(request: Request, response: Response){
-        const { title, description, user_id } = request.body;
+        const { title, description, user_id, thumb, views, time, category, channel, avatar } = request.body;
         pool.getConnection((err: any, connection: any) => {
             connection.query(
-                'INSERT INTO videos (video_id, user_id, title, description) VALUES (?,?,?,?)',
-                [uuidv4(), user_id, title, description],
+                'INSERT INTO videos (video_id, user_id, title, description, thumb, views, time, category, channel, avatar) VALUES (?,?,?,?,?,?,?,?,?,?)',
+                [uuidv4(), user_id, title, description, thumb, views, time, category, channel, avatar],
                 (error: any, result: any, fields: any) => {
                     connection.release();
                     if (error) {
